ea2f7e44f6ecc51fc775f96596abbad93c52a62d
ltg <ltg@bbangyanetworks.com>
2022-07-27 23:06:37 +0900

    web-vitals 제거

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./index.css"
-import "typeface-raleway"
 import Home from "./pages/home"
 import About from "./pages/about"
 import Post from "./pages/post"
@@ -10,14 +9,12 @@ import NotFound from "./pages/notfound"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
-      <Route index path="/" element={<Home />} />
+      <Route index exec path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/post/:id" element={<Post />} />
       <Route path="404" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 )
-
-// reportWebVitals();
