673c89c711e8ee423df6ad7eaf39153258137f25
ltg <ltg@bbangyanetworks.com>
2022-07-30 17:10:33 +0900

    manifest.json 삭제

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Home from "./pages/home"
 import About from "./pages/about"
 import Post from "./pages/post"
 import NotFound from "./pages/notfound"
-
+console.log("hello")
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
