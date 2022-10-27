e00f4ad880073b522086046d3e7f095dc98a65b1
ltg <ltg@bbangyanetworks.com>
2022-10-27 23:37:18 +0900

    nojekyll 파일 추가

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import Home from "./pages/home"
 import About from "./pages/about"
 import Post from "./pages/post"
 import NotFound from "./pages/notfound"
-console.log("hello")
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
